89d0100ebce6147c3dbc65912a4b595b185740b7
Hayden Eastwell <42878077+haydende@users.noreply.github.com>
2020-08-12 17:21:26 +0100

    Added creation of a new ActiveMQ server instance.

--- src/main/java/haydende/jmspractice/JmsPracticeApplication.java
+++ src/main/java/haydende/jmspractice/JmsPracticeApplication.java
@@ -1,12 +1,25 @@
 package haydende.jmspractice;
 
+import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
+import org.apache.activemq.artemis.core.server.ActiveMQServer;
+import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JmsPracticeApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        // create ActiveMQ server instance
+        ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
+            .setPersistenceEnabled(false)
+            .setJournalDirectory("target/data/journal")
+            .setSecurityEnabled(false)
+            .addAcceptorConfiguration("invm", "vm://1"));
+
+        // start the server
+        server.start();
         SpringApplication.run(JmsPracticeApplication.class, args);
     }
 
