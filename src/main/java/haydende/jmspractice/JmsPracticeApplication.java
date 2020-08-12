284f5e5468ce528fc9a71964ae73fa2b641d8630
Hayden Eastwell <42878077+haydende@users.noreply.github.com>
2020-08-12 16:49:04 +0100

    Added initial Spring Boot project

---
+++ src/main/java/haydende/jmspractice/JmsPracticeApplication.java
@@ -0,0 +1,13 @@
+package haydende.jmspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmsPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/haydende/jmspractice/JmsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package haydende.jmspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
